ff4e89a4b341b35464b99ca307d56184172b3528
Helin Shiah <helin.shiah@gmail.com>
2014-09-14 16:03:15 -0500

    basic user model works with secure passwords, but I'm not sure why setting up bcrypt required changing a lot in the user model (adding password and password_confirmation variables), maybe version of rails and ruby?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,16 +25,20 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
     end
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /users/1
@@ -69,6 +73,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :password_digest)
     end
 end
