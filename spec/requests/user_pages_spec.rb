8b40d75803e26e548f863223fe69f5c50b9a6807
Helin Shiah <helin.shiah@gmail.com>
2014-09-14 16:42:52 -0500

    additional tests and signup ready

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 describe "User pages" do
   subject { page}
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_content('Sign up') }
-    it { should have_title('Sign up') }
-  end
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
@@ -15,5 +9,37 @@ describe "User pages" do
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+  end
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title('Sign up') }
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+
+    let(:submit) { "Create User" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
