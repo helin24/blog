23e1f5feee8e32f2f2563645d37d21877189f3c3
Helin <helin.shiash@gmail.com>
2014-09-14 12:57:28 -0500

    layout adjustments - not sure why test for about title is not working with about_path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,39 +13,38 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe "Static Pages" do
+	
 	describe "Home Page" do 
+		before { visit root_path }
 		it "should have the content 'Blog'" do
-			visit '/static_pages/home'
 			expect(page).to have_content('Blog')
 		end
 
 		it "should have title with the word 'Home'" do
-			visit '/static_pages/home'
 			expect(page).to have_title('Home')
 		end
 	end
 
 	describe "Help Page" do 
+		before {visit help_path}
 		it "should have the content 'Help'" do 
-			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
 
 		it "should have title with the word 'Help'" do
-			visit '/static_pages/help'
 			expect(page).to have_title('Help')
 		end
 	end
 
 	describe "About Page" do
+		before {visit about_path}
 		it "should have the content 'About'" do 
-			visit '/static_pages/about'
 			expect(page).to have_content('About')
 		end
 	end
 
 		it "should have title with the word 'About'" do
-			visit '/static_pages/about'
+			visit '/about'
 			expect(page).to have_title('About')
 		end
 
