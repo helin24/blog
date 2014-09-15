9f10fa4587d01a3cf6a4499fb7934d587f82cee4
Helin <helin.shiash@gmail.com>
2014-09-14 20:40:28 -0500

    added ability to post blog on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+    @post = current_user.posts.build if signed_in?
   end
 
   # GET /users/new
